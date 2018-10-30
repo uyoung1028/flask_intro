6c05e8b62681bf102ddb0a4c17dee2dfa5e33f19
uyoung <uyoung1028@gmail.com>
2018-10-30 01:40:26 +0000

    플라스크 기본설정

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+    
+@app.route("/welcome")
+def welcome():
+    return "welcome flask!!"
+    
+@app.route("/html_tag")
+def html_tag():
+    return "<h1>안녕하세요</h1>"
+    
+@app.route("/html_line")
+def html_line():
+    return """
+    <h1>여러줄을 보내봅시다</h1>
+    <ul>
+        <li>1번</li>
+        <li>2번</li>
+    </ul>
+    """
