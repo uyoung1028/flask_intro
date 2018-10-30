07aca92dca24c1a826c1833febfe885c8b6b5d3b
uyoung <uyoung1028@gmail.com>
2018-10-30 02:31:59 +0000

    플라스크 url 설정

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
@@ -23,5 +23,15 @@ def html_line():
     </ul>
     """
     
+@app.route("/html_file")
+def html_file():
+    return render_template("file.html")
     
+@app.route("/hello_p/<string:name>")
+def hello_p(name):
+    return render_template("hello.html", people_name=name)
     
+@app.route("/cube/<int:num>")
+def cube(num):
+    result = num**3
+    return render_template("cube.html", result=result, num=num)
